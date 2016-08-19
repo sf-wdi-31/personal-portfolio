6b16d9354a0dc6b0d4ad262e6aec0d7a2c588d1e
wnamen <williamjosephnamen@gmail.com>
2016-08-19 10:38:51 -0700

    adjusted nav bar jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,15 @@ console.log("app.js linked.");
 $(document).ready(function(){
 
   $(window).on("scroll", function handleScroll(){
-    if ($(this).scrollTop() > 300) {
+    if ($(this).scrollTop() < 150) {
+      $('#header-info').slideDown("slow");
+    } else {
       $('#header-info').slideUp("slow");
     }
   })
 
   $('header').on("mouseover", function handleHover () {
-    $('#header-info').slideDown("slow");
+    $('#header-info').delay(800).slideDown("slow");
   })
 
 })
