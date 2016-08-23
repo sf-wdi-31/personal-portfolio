da6a321c4888575eb82d3cbc3d5e4302b8b5ebfb
wnamen <williamjosephnamen@gmail.com>
2016-08-23 12:21:17 -0700

    created slide toggle for my work section

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,10 @@ console.log("app.js linked.");
 
 $(document).ready(function(){
 
+  $("#apps").css("display", "none");
+  $("#exercises").css("display", "none");
+
+
   $(window).on("scroll", function handleScroll(){
     if ($(this).scrollTop() < 150) {
       $('#header-info').slideDown("slow");
@@ -22,6 +26,20 @@ $(document).ready(function(){
   $('header').mouseleave(function () {
     clearTimeout(time);
   })
+
+
+  $( "#apps-title" ).click(function() {
+    $( "#apps" ).slideToggle( "slow", function() {
+      // Animation complete.
+    });
+  });
+
+  $( "#exer-title" ).click(function() {
+    $( "#exercises" ).slideToggle( "slow", function() {
+      // Animation complete.
+    });
+  });
+
 
 })
 
