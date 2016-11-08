c33caa503d5eae8c44616bc28055dd986a474c4f
wnamen <williamjosephnamen@gmail.com>
2016-11-08 13:40:40 -0800

    revamped website

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,35 +3,35 @@ console.log("app.js linked.");
 
 $(document).ready(function(){
 
-  $("#apps").css("display", "none");
-  $("#exercises").css("display", "none");
-
-  $(window).on("scroll", function handleScroll(){
-    if ($(this).scrollTop() < 150) {
-      $('#header-info').slideDown("slow");
-    } else {
-      $('#header-info').slideUp("slow");
-    }
-  })
-
-  var time;
-
-  $('header').mouseenter(function handleHover () {
-    time = setTimeout (function () {
-      $('#header-info').slideDown("slow")}, 2000);
-  })
-
-  $('header').mouseleave(function () {
-    clearTimeout(time);
-  })
-
-  $( "#apps-title" ).click(function(event) {
-    event.preventDefault();
-    $( "#apps" ).slideToggle("slow");
-  });
-
-  $( "#exer-title" ).click(function(event) {
-    event.preventDefault();
-    $( "#exercises" ).slideToggle( "slow");
-  });
+  // $("#apps").css("display", "none");
+  // $("#exercises").css("display", "none");
+  //
+  // $(window).on("scroll", function handleScroll(){
+  //   if ($(this).scrollTop() < 150) {
+  //     $('#header-info').slideDown("slow");
+  //   } else {
+  //     $('#header-info').slideUp("slow");
+  //   }
+  // })
+  //
+  // var time;
+  //
+  // $('header').mouseenter(function handleHover () {
+  //   time = setTimeout (function () {
+  //     $('#header-info').slideDown("slow")}, 2000);
+  // })
+  //
+  // $('header').mouseleave(function () {
+  //   clearTimeout(time);
+  // })
+  //
+  // $( "#apps-title" ).click(function(event) {
+  //   event.preventDefault();
+  //   $( "#apps" ).slideToggle("slow");
+  // });
+  //
+  // $( "#exer-title" ).click(function(event) {
+  //   event.preventDefault();
+  //   $( "#exercises" ).slideToggle( "slow");
+  // });
 })
